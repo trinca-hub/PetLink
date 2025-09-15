0f91e0a19c376506e509d030cf1ac64c5975179e
trinca-hub <oliveiratrinca@gmail.com>
2025-09-15 13:29:00 -0300

    pasta bin apagada

--- PetLink-BackEnd/PetLink-BackEnd/obj/Debug/net8.0/PetLink-BackEnd.AssemblyInfo.cs
+++ PetLink-BackEnd/PetLink-BackEnd/obj/Debug/net8.0/PetLink-BackEnd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetLink-BackEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3489be06ac940119e25f9bdd779449a0774a3e5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+032be6e31447e95b85012ec159b6f78da88ec31a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetLink-BackEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetLink-BackEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
